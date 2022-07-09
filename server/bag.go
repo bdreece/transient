706ceef8ba759188f8ada262c52409db52f6857d
Brian Reece <bdreece@mtu.edu>
2022-07-09 13:43:17 -0400

    Implemented Bag wrapper for RadixTrie

--- server/bag.go
+++ server/bag.go
@@ -2,7 +2,7 @@ package main
 
 type BagItem[T any] struct {
 	Count int
-	Value T
+	Value *T
 }
 
 type Bag[T any] struct {
@@ -13,4 +13,31 @@ func NewBag[T any]() Bag[T] {
 	return Bag[T]{
 		store: NewRadixTrie[BagItem[T]](),
 	}
+}
+
+func (b *Bag[T]) Push(key string, count int, value *T) {
+	item := b.store.Get(key)
+	if item == nil {
+		b.store.Set(key, &BagItem[T]{
+			Count: count,
+			Value: value,
+		})
+	} else {
+		item.Count += count
+		if value != nil {
+			item.Value = value
+		}
+	}
+}
+
+func (b *Bag[T]) Pop(key string) *T {
+	item := b.store.Get(key)
+	if item == nil {
+		return nil
+	}
+	item.Count -= 1
+	if item.Count == 0 {
+		b.store.Del(key)
+	}
+	return item.Value
 }
