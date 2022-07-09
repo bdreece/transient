a84eacd1c79843470ea5cd6d5155a09d36e46d39
Brian Reece <bdreece@mtu.edu>
2022-07-09 09:27:14 -0400

    Added global fonts and Vue router

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,51 @@
-import {createApp} from 'vue'
-import {createRouter, createWebHistory} from 'vue-router'
-import App from './App.vue'
-import Index from './pages/Index.vue'
-import Search from './pages/Search.vue'
-import Song from './pages/Song.vue'
-import Upload from './pages/Upload.vue'
-import "./main.css"
+/* Transient - A temporary audio file sharing service
+ * Copyright (C) 2022 Brian Reece
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+
+import App from './App.vue';
+import Index from './pages/Index.vue';
+import Song from './pages/Song.vue';
+import Upload from './pages/Upload.vue';
+
+import './main.css';
 
 const routes = [
-    {path: '/', component: Index},
-    {path: '/search', component: Search},
-    {path: '/upload', component: Upload},
-    {path: '/:id', component: Song},
-]
+  { path: '/', component: Index },
+  { path: '/upload', component: Upload },
+  { path: '/songs/:id', component: Song },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-})
+  history: createWebHistory(),
+  routes,
+});
 
-const app = createApp(App)
+/* add icons to the library */
+library.add(faUserSecret);
 
-app.use(router)
+/* add font awesome icon component */
+const app = createApp(App);
 
-app.mount('#app')
+app.config.unwrapInjectedRef = true;
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.use(router);
+
+app.mount('#app');
