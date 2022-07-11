66a44ca02b7fdbb9ce7a5bb800dab5c4fbcbe7b0
Brian Reece <bdreece@mtu.edu>
2022-07-10 22:16:50 -0400

    Attempted fix of upload component

--- src/pages/Upload.vue
+++ src/pages/Upload.vue
@@ -1,52 +1,96 @@
-<script setup lang="ts">
-import cuid from 'cuid';
-import { ref } from 'vue';
+<script lang="ts">
+/* Transient - A temporary audio file sharing service
+ * Copyright (C) 2022 Brian Reece
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import { Buffer } from 'buffer';
+import { defineComponent } from 'vue';
 
 const API_HOST = 'localhost:8080';
 
-const trackName = ref('');
-const artistName = ref('');
-const description = ref('');
-const file = ref('');
-const format = ref('');
-const remainingPlays = ref(1);
-
-const handleFileChange = async ({ target }: Event) => {
-  const files = (target as HTMLInputElement).files;
-  if (files) {
-    const data = await files[0].arrayBuffer();
-    format.value = files[0].type;
-    file.value = Buffer.from(data).toString('base64');
-  }
-};
-
-const handleUpload = async () => {
-  const id = cuid.slug();
-  const response = await fetch(`http://${API_HOST}/api/songs/${id}`, {
-    method: 'OPTIONS',
-    headers: {
-      'Access-Control-Request-Method': 'POST',
-      Origin: origin,
+export default defineComponent({
+  data() {
+    return {
+      trackName: '',
+      artistName: '',
+      description: '',
+      file: '',
+      format: '',
+      remainingPlays: 1,
+    };
+  },
+  computed: {
+    showUpload() {
+      return (
+        this.trackName != '' &&
+        this.artistName != '' &&
+        this.file != '' &&
+        this.format != ''
+      );
     },
-    body: JSON.stringify({
-      trackName: trackName.value,
-      artistName: artistName.value,
-      description: description.value,
-      file: file.value,
-      format: format.value,
-      remainingPlays: remainingPlays.value,
-    }),
-  });
-  if (response.ok) {
-    // TODO: File uploaded successfully, display success modal
-    const data = await response.text();
-    const { id } = JSON.parse(data);
-    console.log(`File uploaded successfully: ${id}`);
-  } else {
-    // TODO: File failed to upload, display failure modal
-    console.log('File failed to upload');
-  }
-};
+  },
+  methods: {
+    async handleFileChange({ target }: Event) {
+      const files = (target as HTMLInputElement).files;
+
+      if (files) {
+        const data = await files[0].text();
+        this.format = files[0].type;
+        this.file = Buffer.from(data).toString('base64');
+      }
+    },
+    async handleUpload() {
+      const {
+        trackName,
+        artistName,
+        description,
+        file,
+        format,
+        remainingPlays,
+      } = this;
+
+      const response = await fetch(`http://${API_HOST}/api/songs`, {
+        method: 'POST',
+        headers: {
+          'Access-Control-Request-Method': 'POST',
+          'Content-Type': 'application/json',
+          Origin: origin,
+        },
+        body: JSON.stringify({
+          trackName,
+          artistName,
+          description,
+          file,
+          format,
+          remainingPlays,
+        }),
+      });
+
+      if (response.ok) {
+        // TODO: File uploaded successfully, display success modal
+        const data = await response.text();
+        console.log(`Raw response: ${data}`);
+        const { id } = JSON.parse(data);
+        console.log(`File uploaded successfully: ${id}`);
+      } else {
+        // TODO: File failed to upload, display failure modal
+        console.log('File failed to upload');
+      }
+    },
+  },
+});
 </script>
 
 <template>
@@ -127,7 +171,7 @@ const handleUpload = async () => {
       </div>
       <div class="card-actions justify-center mb-2">
         <button
-          v-show="file.size > 0"
+          v-show="showUpload"
           class="btn btn-primary"
           @click="handleUpload"
         >
