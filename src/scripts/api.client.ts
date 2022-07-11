dac5e728f682af00ac227f1d24ee41f1c9a8c301
Brian Reece <bdreece@mtu.edu>
2022-07-11 12:12:41 -0400

    Updated client-side song data schema

--- src/scripts/api.client.ts
+++ src/scripts/api.client.ts
@@ -1,10 +1,15 @@
 export const API_HOST = 'localhost:8080';
 
+export interface File {
+  data: string;
+  format: string;
+}
+
 export interface Song {
   trackName: string;
   artistName: string;
   description?: string;
-  file: string;
-  format: string;
+  image?: File;
+  audio: File;
   remainingPlays: number;
 }
