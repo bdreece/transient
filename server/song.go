7d8ccf92d0168255bf8e07081c0c9eaf1aef197b
Brian Reece <bdreece@mtu.edu>
2022-07-11 15:23:57 -0400

    Implemented dataurl transformations for SongData

--- server/song.go
+++ server/song.go
@@ -18,17 +18,32 @@ package main
 
 import (
 	"fmt"
+	"mime"
 	"os"
+
+	"github.com/vincent-petithory/dataurl"
 )
 
 type FileData struct {
-	Data   []byte `json:"data"`
+	Data   string `json:"data"`
 	Format string `json:"format"`
 }
 
-func (d FileData) Store(filePath *string, id string, fileType string) (*FileStore, error) {
-	path := fmt.Sprintf("%s/%s-%s", *filePath, id, fileType)
-	err := os.WriteFile(path, d.Data, 0744)
+func (d FileData) Store(filePath *string, id string) (*FileStore, error) {
+	// Decode data URL
+	data, err := dataurl.DecodeString(d.Data)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get file extension
+	ext, err := mime.ExtensionsByType(data.MediaType.ContentType())
+	if err != nil {
+		return nil, err
+	}
+
+	path := fmt.Sprintf("%s/%s.%s", *filePath, id, ext[0])
+	err = os.WriteFile(path, data.Data, 0744)
 	if err != nil {
 		return nil, err
 	}
@@ -51,13 +66,13 @@ func NewSongData() (s SongData) { return }
 
 func (s SongData) Store(filePath *string, id string) (*SongStore, error) {
 	// Store image
-	image, err := s.Image.Store(filePath, id, "image")
+	image, err := s.Image.Store(filePath, id)
 	if err != nil {
 		return nil, err
 	}
 
 	// Store audio
-	audio, err := s.Audio.Store(filePath, id, "audio")
+	audio, err := s.Audio.Store(filePath, id)
 	if err != nil {
 		return nil, err
 	}
@@ -84,7 +99,7 @@ func (s FileStore) Data() (*FileData, error) {
 	}
 
 	return &FileData{
-		Data:   data,
+		Data:   dataurl.EncodeBytes(data),
 		Format: s.Format,
 	}, nil
 }
