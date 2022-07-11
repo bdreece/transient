5863f9189c37ba3696160916a880d7996c5adafb
Brian Reece <bdreece@mtu.edu>
2022-07-11 12:02:51 -0400

    Split Song implementation into SongData and SongStore

--- server/song.go
+++ server/song.go
@@ -16,13 +16,104 @@
  */
 package main
 
-type Song struct {
-	TrackName      string `json:"trackName"`
-	ArtistName     string `json:"artistName"`
-	Description    string `json:"description"`
-	File           []byte `json:"file"`
-	Format         string `json:"format"`
-	RemainingPlays int    `json:"remainingPlays"`
+import (
+	"fmt"
+	"os"
+)
+
+type FileData struct {
+	Data   []byte `json:"data"`
+	Format string `json:"format"`
 }
 
-func NewSong() (s Song) { return }
+func (d FileData) Store(id string, fileType string) (*FileStore, error) {
+	path := fmt.Sprintf("%s/%s-%s", DATA_PATH, id, fileType)
+	err := os.WriteFile(path, d.Data, 0666)
+	if err != nil {
+		return nil, err
+	}
+	return &FileStore{
+		Path:   path,
+		Format: d.Format,
+	}, nil
+}
+
+type SongData struct {
+	TrackName      string   `json:"trackName"`
+	ArtistName     string   `json:"artistName"`
+	Description    string   `json:"description"`
+	Image          FileData `json:"image"`
+	Audio          FileData `json:"audio"`
+	RemainingPlays int      `json:"remainingPlays"`
+}
+
+func NewSongData() (s SongData) { return }
+
+func (s SongData) Store(id string) (*SongStore, error) {
+	// Store image
+	image, err := s.Image.Store(id, "image")
+	if err != nil {
+		return nil, err
+	}
+
+	// Store audio
+	audio, err := s.Audio.Store(id, "audio")
+	if err != nil {
+		return nil, err
+	}
+
+	return &SongStore{
+		TrackName:      s.TrackName,
+		ArtistName:     s.ArtistName,
+		Description:    s.Description,
+		Image:          *image,
+		Audio:          *audio,
+		RemainingPlays: s.RemainingPlays,
+	}, nil
+}
+
+type FileStore struct {
+	Path   string `json:"path"`
+	Format string `json:"path"`
+}
+
+func (s FileStore) Data() (*FileData, error) {
+	data, err := os.ReadFile(s.Path)
+	if err != nil {
+		return nil, err
+	}
+
+	return &FileData{
+		Data:   data,
+		Format: s.Format,
+	}, nil
+}
+
+type SongStore struct {
+	TrackName      string    `json:"trackName"`
+	ArtistName     string    `json:"artistName"`
+	Description    string    `json:"description"`
+	Image          FileStore `json:"image"`
+	Audio          FileStore `json:"audio"`
+	RemainingPlays int       `json:"remainingPlays"`
+}
+
+func (s SongStore) Data() (*SongData, error) {
+	image, err := s.Image.Data()
+	if err != nil {
+		return nil, err
+	}
+	audio, err := s.Audio.Data()
+	if err != nil {
+		return nil, err
+	}
+
+	return &SongData{
+		TrackName:      s.TrackName,
+		ArtistName:     s.ArtistName,
+		Description:    s.Description,
+		Image:          *image,
+		Audio:          *audio,
+		RemainingPlays: s.RemainingPlays,
+	}, nil
+}
