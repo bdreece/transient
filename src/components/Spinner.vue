3f8792a9c3905b03c84eb01753502d502e40a74b
Brian Reece <bdreece@mtu.edu>
2022-07-11 15:24:07 -0400

    Extracted Spinner into component

---
+++ src/components/Spinner.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="spinner">
+    <div class="rect1" />
+    <div class="rect2" />
+    <div class="rect3" />
+    <div class="rect4" />
+    <div class="rect5" />
+  </div>
+</template>
