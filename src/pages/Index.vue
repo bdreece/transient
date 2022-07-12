ff5ca51ca5162542c8b3711e27e063351a4167d4
Brian Reece <bdreece@mtu.edu>
2022-07-11 21:15:37 -0400

    Fixed Index page responsiveness

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -22,7 +22,7 @@ export default defineComponent({});
 
 <template>
   <div class="flex flex-col">
-    <div class="hero py-12 bg-base-200">
+    <div class="hero py-8 bg-base-200">
       <div class="hero-content text-center">
         <div class="max-w-md">
           <h1 class="text-5xl font-bold heading">Audio sharing, unlisted</h1>
@@ -33,27 +33,44 @@ export default defineComponent({});
             create temporary, unlisted links to your audio files for others to
             consume.
           </p>
-          <a class="btn btn-primary" href="#get-started">Get Started</a>
+          <div class="flex justify-center">
+            <a class="btn btn-primary mx-4" href="#get-started">Get Started</a>
+            <router-link class="btn btn-accent mx-4" to="/upload"
+              >Upload Now</router-link
+            >
+          </div>
         </div>
       </div>
     </div>
-    <div class="grid grid-cols-2 steps steps-vertical mx-56">
+    <div class="flex flex-col justify-center align-evenly">
+      <ul id="get-started" class="steps sticky top-16 z-50 py-8 bg-base-100">
+        <li class="step step-primary">
+          <a href="#upload"> Upload </a>
+        </li>
+        <li class="step step-accent">
+          <a href="#configure"> Configure </a>
+        </li>
+        <li class="step step-success">
+          <a href="#share"> Share </a>
+        </li>
+      </ul>
       <!-- TODO: Fix this for mobile -->
-      <div id="get-started" class="step">Upload</div>
-      <p class="my-8">
-        Visit the
-        <router-link class="link link-primary" to="/upload">Upload</router-link>
-        page to upload your track or stem. Only one audio file per upload is
-        supported at the time of writing this. Supported filetypes are MP3, WAV,
-        AIFF, AAC, and OGG.
+      <h2 id="upload" class="flex justify-center text-2xl heading">Upload</h2>
+      <p class="flex justify-center m-12">
+        Visit the Upload page to upload your track or stem. Only one audio file
+        per upload is supported at the time of writing this. Supported filetypes
+        are MP3, WAV, AIFF, AAC, and OGG.
       </p>
-      <div class="step">Configure</div>
-      <p class="my-8">
-        Configure your upload with the desired expiration time and/or maximum
-        number of plays to control when your track is removed.
+      <h2 id="configure" class="flex justify-center text-2xl heading">
+        Configure
+      </h2>
+      <p class="flex justify-center m-12">
+        Configure your upload with a track name, artist name, album art, and
+        maximum plays. Upon exhausting the maximum number of plays, the track
+        will be automatically deleted from our database.
       </p>
-      <div class="step">Share</div>
-      <p class="my-8">
+      <h2 id="share" class="flex justify-center text-2xl heading">Share</h2>
+      <p class="flex justify-center m-12">
         Upon successful upload, we will provide you with a link to your track
         that is only valid per the settings provided in the Configuration step.
         Share this link with friends or other artists so they can download your
